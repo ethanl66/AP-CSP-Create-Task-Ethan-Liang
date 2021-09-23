8df75d1af0aedb581c2ce726ee21fae65df9d079
Ehan <ethanliang405@gmail.com>
2021-09-23 12:30:39 -0400

    do history now

---
+++ index.js
@@ -0,0 +1,151 @@
+console.log("hi");
+
+const historyArray = [];
+const imgCategory = [
+  "waifu",
+  "neko",
+  "shinobu",
+  "megumin",
+  "bully",
+  "cry",
+  "hug",
+  "awoo",
+  "kiss",
+  "lick",
+  "pat",
+  "smug",
+  "bonk",
+  "yeet",
+  "blush",
+  "smile",
+  "wave",
+  "highfive",
+  "nom",
+  "bite",
+  "slap",
+  "kick",
+  "happy",
+  "wink",
+  "poke",
+  "cringe",
+];
+
+let chooseCategory = function () {
+  let selectionNumber = Math.floor(Math.random() * 90 + 1);
+  //console.log(selectionNumber);
+
+  if (selectionNumber == 1) {
+    category = "cringe";
+  }
+  if (selectionNumber == 13 || selectionNumber == 14) {
+    category = "poke";
+  }
+  if (selectionNumber == 2) {
+    category = "wink";
+  }
+  if (selectionNumber == 3) {
+    category = "happy";
+  }
+  if (selectionNumber == 15 || selectionNumber == 16) {
+    category = "kick";
+  }
+  if (selectionNumber == 17 || selectionNumber == 18) {
+    category = "slap";
+  }
+  if (selectionNumber == 4) {
+    category = "bite";
+  }
+  if (selectionNumber == 5) {
+    category = "nom";
+  }
+  if (selectionNumber == 6) {
+    category = "highfive";
+  }
+  if (selectionNumber == 7) {
+    category = "wave";
+  }
+  if (selectionNumber == 19 || selectionNumber == 20) {
+    category = "smile";
+  }
+  if (selectionNumber == 8) {
+    category = "blush";
+  }
+  if (selectionNumber == 21 || selectionNumber == 22) {
+    category = "yeet";
+  }
+  if (selectionNumber == 23 || selectionNumber == 24) {
+    category = "bonk";
+  }
+  if (selectionNumber > 24 && selectionNumber < 33) {
+    category = "smug";
+  }
+  if (selectionNumber == 9) {
+    category = "pat";
+  }
+  if (selectionNumber == 10) {
+    category = "lick";
+  }
+  if (selectionNumber > 32 && selectionNumber < 44) {
+    category = "awoo";
+  }
+  if (selectionNumber == 11) {
+    category = "hug";
+  }
+  if (selectionNumber == 12) {
+    category = "cry";
+  }
+  if (selectionNumber == 44 || selectionNumber == 45) {
+    category = "bully";
+  }
+  if (selectionNumber > 45 && selectionNumber < 55) {
+    category = "megumin";
+  }
+  if (selectionNumber > 54 && selectionNumber < 64) {
+    category = "shinobu";
+  }
+  if (selectionNumber > 63 && selectionNumber < 72) {
+    category = "neko";
+  }
+  if (selectionNumber > 71 && selectionNumber < 91) {
+    category = "waifu";
+  }
+  console.log(category);
+};
+
+let fetchData = async function () {
+  chooseCategory();
+
+  try {
+    const response = await fetch(`https://api.waifu.pics/sfw/${category}`);
+    const data = await response.json();
+    //console.log(data.url);
+
+    historyArray.push(data.url);
+    console.log(historyArray);
+
+    document.getElementById(
+      "main-img-div"
+    ).innerHTML = `<img id='main-img' src="${data.url}" alt="Waifu pic">`;
+
+    /*     document.getElementById(
+      "history-img-div"
+    ).innerHTML += `<img class='hist-img' src="${
+      historyArray[historyArray.length - 2]
+    }" alt="Waifu pic">`; */
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+};
+
+document.getElementById("roll-button").addEventListener("click", function () {
+  fetchData();
+  //for each link in the history array, insertHTML image into history div
+
+  /* historyArray.forEach(
+    (link) =>
+      (document.getElementById(
+        "history-img-div"
+      ).innerHTML += `<img class='hist-img' src="${link}" alt="Waifu pic">`)
+  ); */
+});
